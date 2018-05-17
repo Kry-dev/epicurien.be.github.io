9392f103a2b36b62743fe3e40dbd048c276d394c
kry-dev <yaroslavkryvda@gmail.com>
2018-05-17 12:30:30 +0300

    add scroll to top buttom

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,20 @@ var $ = require('jquery');
 var slick = require('slick-carousel');
 
 $(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $("#toTop").fadeIn();
+        } else {
+            $("#toTop").fadeOut();
+        }
+        if($(window).scrollTop() + $(window).height() < $(document).height() - $("#footer").height()) {
+            $('#toTop').css("position","fixed");    //resetting it
+            $('#toTop').css("bottom","0"); //resetting it
+        }
+    });
 
+
+    $("#toTop").click(function() {
+        $("html, body").animate({scrollTop: 0}, 1000);
+    });
 });
